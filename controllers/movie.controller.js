0c43dfa666596fbe940273bbfc8087c4fe2760fe
Karan Mishra <karan206245mishra@gmail.com>
2026-03-22 20:40:17 +0530

    updated the controller logic

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -15,7 +15,7 @@ const createMovie=async (req,res)=>{
             message:'Successfully created a new movie'
         })
     }
-    catch(err){
+    catch(err){ 
         console.log(err);
         return res.status(500).json({
             success:true,
@@ -26,6 +26,51 @@ const createMovie=async (req,res)=>{
     }
 };
 
+const deleteMovie=async(req,res)=>{
+    try{
+        const response=await Movie.deleteOne({
+            _id:req.params.id
+        });
+        return res.status(200).json({
+            success:true,
+            error:{},
+            message:'Successfully deleted the movie',
+            data:response
+        });
+    } catch(err){
+        console.log(err);
+        return res.status(500).json({
+            success:false,
+            error:err,
+            message:'Somethibg webt wrong',
+            data:{}
+        });
+    }
+}
+
+const getMovie=async(req,res)=>{
+    try{
+        const movie=await Movie.findById(req.params.id);
+        return res.status(200).json({
+            success:true,
+            error:{},
+            message:'Successfully fetched the movie details',
+            data:movie
+        });
+    } catch(err){
+        console.log(err);
+        return res.status(500).json({
+            success:false,
+            error:err,
+            message:'Something went wrong',
+            data:{}
+        })
+
+    }
+}
+
 module.exports={
-    createMovie
+    createMovie,
+    deleteMovie,
+    getMovie
 }
