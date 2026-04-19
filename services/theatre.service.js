5049184989957e3a75e380ca2c9a2e07143420f9
Karan Mishra <karan206245mishra@gmail.com>
2026-04-19 22:00:30 +0530

    Feat:updated theatre service

--- services/theatre.service.js
+++ services/theatre.service.js
@@ -54,8 +54,15 @@ const getAllTheatres=async(data)=>{
     try{
         let query={};  
         if(data && data.city){
+            //this checks whether the city is present in query params or not
             query.city=data.city;     
         }
+        if(data && data.pincode){
+            query.pincode=data.pincode;
+        }
+        if(data && data.name){
+            query.name=data.name;
+        } 
         const response=await Theatre.find(query);
         return response;
     } catch(error){
@@ -110,7 +117,7 @@ const updateMoviesInTheatres=async(theatreId, movieIds,insert)=>{
         theatre.movies=savedMovieIds;
     }
     await theatre.save();
-    return theatre;
+    return theatre.populate('movies');
 }
  
 module.exports={
